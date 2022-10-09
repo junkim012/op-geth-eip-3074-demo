a9a2f7a2c01bcd4399e7520779b4c65a7bff8001
Jun Kim <64379343+junkim012@users.noreply.github.com>
2022-10-09 07:39:45 -0500

    demo buttons

--- src/components/TxBlock.jsx
+++ src/components/TxBlock.jsx
@@ -1,4 +1,4 @@
-import { ethers, Signer } from 'ethers'
+import { ethers, Signer, BigNumber } from 'ethers'
 import { parseEther } from 'ethers/lib/utils';
 import React, { useEffect, useState } from 'react'
 import { useContract, useSigner, usePrepareSendTransaction, useSendTransaction, useWaitForTransaction } from 'wagmi';
@@ -10,6 +10,8 @@ export default function TxBlock() {
 
     const { data: signer } = useSigner();
 
+    const [storageVal, setStorageVal] = useState("hi");
+
     const [to, setTo] = React.useState('');
     const [amount, setAmount] = React.useState('');
 
@@ -19,44 +21,11 @@ export default function TxBlock() {
     console.log('debouncedTo: ', debouncedTo);
     console.log('debouncedValue: ', debouncedValue);
 
-    // const contract = useContract({
-    //     addressOrName: '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-    //     contractInterface: OwnMeABI,
-    //     signerOrProvider: signer
-    // })
-
-    // const { config } = usePrepareSendTransaction({
-    //     request: {
-    //         to: debouncedTo,
-    //         value: debouncedValue ? parseEther(debouncedValue) : undefined
-    //     }
-    // })
-
-    // console.log('config: ', config); 
-
-    // const { data, sendTransaction } = useSendTransaction(config);
-
-    // const { isLoading, isSuccess } = useWaitForTransaction({
-    //     hash: data?.hash,
-    // })
 
 
-    // const executeSend = async (e) => {
-    //     e.preventDefault();
-
-    //     console.log('data: ', data);
-    //     console.log('sendTransaction: ', sendTransaction);
-    //     console.log('executeSend:'); 
-
-    //     const tx = sendTransaction?.();
-    //     console.log('tx: ', tx);
-    // }
-    // const authorize = () => {
-    //     if (!signer || !contract) return; 
-    //     const tx = 
-    // }
     const invokerAddress = "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    const ownAddress = ""
+    const storageAddress = "0x000000000000000000000000000000000000bbbb";
+    // const ownAddress = ""
     const privateKey = "9ad24de184bb9bd3057c3a4625042b26f4ce90f1aef321252f0069c5887e2e54";
 
     async function sponsorCall() {
@@ -67,29 +36,31 @@ export default function TxBlock() {
 
     async function query() {
         if (typeof window.ethereum !== 'undefined') {
-
+            const slot = 0;
             const storageContractAddress = "0x000000000000000000000000000000000000bbbb";
 
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             const wallet = new ethers.Wallet(privateKey);
             const walletSigner = wallet.connect(provider);
 
-            const paddedSlot = utils.hexZeroPad(slot, 32);
-            const storageLocation = await ethers.provider.getStorageAt(storageContractAddress, paddedSlot);
+            const paddedSlot = ethers.utils.hexZeroPad(0, 32);
+            const storageLocation = await provider.getStorageAt(storageContractAddress, paddedSlot);
             console.log('storageLocation: ', storageLocation);
             const storageValue = BigNumber.from(storageLocation);
             console.log('storageValue: ', storageValue);
+            setStorageVal(storageValue._hex);
         }
     }
 
-    async function send(e) {
+
+    // sponsored transaction metamask wallet is the sponsor
+    async function sponsorIndirect(e) {
         e.preventDefault();
         console.log('window.ethereum: ', window.ethereum);
         if (typeof window.ethereum !== 'undefined') {
             console.log('inside window.ethereum');
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             console.log('provider: ', provider);
-            const signer = provider.getSigner();
             const wallet = new ethers.Wallet(privateKey);
             const walletSigner = wallet.connect(provider);
             console.log('wallet: ', wallet);
@@ -100,7 +71,11 @@ export default function TxBlock() {
                 from: wallet.address,
                 to: invokerAddress,
                 value: ethers.utils.parseEther(amount),
-                // nonce: await provider.getTransactionCount(wallet.address, "latest"),
+                // nonce: Math.round(Math.random(1, 1000)+1000),
+                // nonce: 123
+                nonce: await provider.getTransactionCount(wallet.address, "latest"),
+                gasLimit: 10000000
+
             }
             console.log('tx: ', tx);
 
@@ -114,36 +89,64 @@ export default function TxBlock() {
         }
     }
 
+    async function sponsorDirect(e) {
+        e.preventDefault();
+        if (typeof window.ethereum !== 'undefined') {
+            console.log('inside window.ethereum');
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            console.log('provider: ', provider);
+            const wallet = new ethers.Wallet(privateKey);
+            const walletSigner = wallet.connect(provider);
+            console.log('wallet: ', wallet);
+
+            let amount = '1';
+
+            let tx = {
+                from: wallet.address,
+                to: storageAddress,
+                value: ethers.utils.parseEther(amount),
+                nonce: await provider.getTransactionCount(wallet.address, "latest"),
+            }
+            console.log('tx: ', tx);
+
+            const receipt = await walletSigner.sendTransaction(tx);
+            console.log(receipt);
+        }
+    }
+
+
     return (
         <div>
-            <form className="flex flex-col space-y-4"
-                onSubmit={(e) => send(e)}>
-                <input className="text-black"
-                    onChange={(e) => setTo(e.target.value)}
-                    placeholder="wallet address"
-                    value={to}
-                />
-                <input className="text-black"
-                    onChange={(e) => setAmount(e.target.value)}
-                    placeholder="amount"
-                    value={amount}
-                />
-                {/* <button disabled={isLoading || !sendTransaction || !to || !amount}>
-                    {isLoading ? 'Sending...' : 'Send'}
-                </button> */}
-                <button
-                // disabled={!sendTransaction || !to || !amount}
-                >
-                    {/* {isLoading ? 'Authorizing...' : 'Authorize'} */}
-                    Authorize
+            <div className="flex flex-col space-y-4 justify-center text-center">
+              
+                <button 
+                onClick={(e) => sponsorDirect(e)}
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Modify Storage Directly
                 </button>
-            </form>
-            <button onClick={query}>
-                Check Storage Contract
-            </button>
+                <button 
+                onClick={(e) => sponsorIndirect(e)}
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Modify Storage through Invoker
+                </button>
+
+                <button 
+                onClick={query}
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Check Storage Contract
+                </button>
+                <div className="flex flex-col">
+                    <div>
+                        Caller Address/msg.sender: 
+                    </div>
+                    {storageVal}
+                </div>
+        
+           
 
 
         </div>
+        </div >
     )
 
 }
